cfb4e3d446c6ebaf3bbbf9666bd79ea99ae93ce3
frontend33 <vikulov33@rambler.ru>
2018-08-03 16:33:38 +0300

    Добавил исключение

--- js/xhr.js
+++ js/xhr.js
@@ -1,10 +1,22 @@
 'use strict'
 var xhr=new XMLHttpRequest();
 //  что бы убедиться что запрос закончен вешается событие load и видим статус 200 ОК
-// Запрос пишется до send() потому что придти может мгновенно , поэтому в начале пишется обработчик события
-xhr.addEventListener("load", function(){
-  console.log(xhr.readyState)
-  console.log(xhr.status+ " "+ xhr.statusText)
+// Запрос пишется до send() потому что придти может мгновенно , поэтому в начале пишется обработчплавный переход между цветамиик события
+xhr.addEventListener("load", function(evt){
+	try{
+		console.log(JSON.parse(xhr.responseText))
+	} catch(err){
+		console.error(err.message)	
+	}
+
+
+
+  console.log(evt.targret===xhr)
+  // Ответ хранится в responseText
+  console.log(xhr.responseText)
+  // Преобразуем в json объект при помощи JSON.parse 
+  // json parse превратит строку с данными в формате JSON в JavaScript-объект/массив/
+  
 })
 
 xhr.open('GET','https://raw.githubusercontent.com/m0mus/json_demo/master/src/main/resources/jasons.json')
@@ -12,3 +24,10 @@ console.log(xhr.readyState)
 console.log(xhr)
 xhr.send()
 console.log(xhr.readyState)
+
+// Исключение как try except в python
+try{
+	console.log("Hello world"+ noName)
+} catch(error){
+	console.log("Поймали исключение "+ error.message)
+}
